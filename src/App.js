be7eff53db20bd8a1031c48224278c545079e47b
glennanj1 <glennan20@gmail.com>
2021-12-02 16:48:58 -0500

    added functionality for mui grow

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
 import './App.css';
+import Grow from '@mui/material/Grow';
+import React, {useState, useEffect} from 'react';
+import  BasicSpeedDial from './components/SpeedDial.js';
+
 
 function App() {
+  
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    //setChecked((prev) => !prev);
+    console.log('scrolling' + window.scrollY)
+    let position = (window.scrollY);
+    console.log(position);
+    if (position >= 680 && position <= 1600) {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+    console.log(checked);
+  };
+
+  const box = ( 
+              <div className="box">
+                <h1>Test</h1>
+              </div> 
+  )
+
+
+  useEffect(() => {
+     window.addEventListener('scroll', handleChange);
+     return () => {
+        window.removeEventListener("scroll", handleChange);
+      };
+  }, []);
+
+
+  
+
   return (
     <>
-      {/* header */}
       <div className="Wrapper">
         <div className="Section1">
+          <div className="nav">
+            <div className="nav-item">
+              <a href="#">Home</a>
+            </div>
+            <div className="nav-item">
+              <a href="#">Events</a>
+            </div>
+            <div className="nav-item">
+              <a href="#">About Us</a>
+            </div>
+          </div>
           <div className="container">
             <div className="container_heading">
               <h1>Free Sourcez</h1>
@@ -22,20 +69,23 @@ function App() {
         </div>
         <div className="Section2">
           <div className="row_container">
-            <div className="box">
-              <h1>Test</h1>
-            </div>
-            <div className="box">
-              <h1>Test</h1>
-            </div>
-            <div className="box">
-              <h1>Test</h1>
-            </div>
+            <Grow in={checked}>
+              {box}
+            </Grow>
+            <Grow in={checked}>
+              {box}
+            </Grow>
+            <Grow in={checked}>
+              {box}
+            </Grow>
           </div>
         </div>
         <div className="Section3">
-          <h1 style={{color: 'white',}}>CountDown Timer or Countup for Testimonial</h1>
+          <h1 style={{color: 'white'}}>CountDown Timer or Countup for Testimonial</h1>
         </div> 
+        <div className="dial">
+          <BasicSpeedDial />    
+        </div>
       </div>
       {/* footer */}
     </>
@@ -43,3 +93,5 @@ function App() {
 }
 
 export default App;
+
+
