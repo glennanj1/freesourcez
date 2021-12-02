0f2aa341f743131f4600489d07172d9e9c230eee
glennanj1 <glennan20@gmail.com>
2021-12-02 10:33:54 -0500

    working on project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* header */}
+      <div className="Wrapper">
+        <div className="Section1">
+        </div>
+        <div className="Section2">
+        </div>
+        <div className="Section3">
+        </div> 
+      </div>
+      {/* footer */}
+    </>
   );
 }
 
