93adc0524668e1187d6cede701a8a811d41e2858
glennanj1 <glennan20@gmail.com>
2021-12-02 11:53:37 -0500

    started drawing out the form for the project

--- src/App.js
+++ src/App.js
@@ -6,10 +6,35 @@ function App() {
       {/* header */}
       <div className="Wrapper">
         <div className="Section1">
+          <div className="container">
+            <div className="container_heading">
+              <h1>Free Sourcez</h1>
+            </div>
+            <div className="container_subheading">
+              <h2>
+                A place to share your knowledge with the world.
+              </h2>
+            </div>
+            <div className="container_sections">
+              Testimonial Carousel
+            </div>
+          </div>
         </div>
         <div className="Section2">
+          <div className="row_container">
+            <div className="box">
+              <h1>Test</h1>
+            </div>
+            <div className="box">
+              <h1>Test</h1>
+            </div>
+            <div className="box">
+              <h1>Test</h1>
+            </div>
+          </div>
         </div>
         <div className="Section3">
+          <h1 style={{color: 'white',}}>CountDown Timer or Countup for Testimonial</h1>
         </div> 
       </div>
       {/* footer */}
